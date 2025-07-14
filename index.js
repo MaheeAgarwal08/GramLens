1e3eff70cc11ca7d0a706491b03a50244fda48b4
MaheeAgarwal08 <agarwalmahee08@gmail.com>
2025-07-14 17:57:58 +0530

    Hosting

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const path=require("path");
 
 const app=express();
 
-const port=3000;
+const port = process.env.PORT || 3000;
+
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
